2b42d328166bcc36fd67036d4cac0c965b7da979
20161104568 <a20161104568@Mac02.local>
2018-10-10 10:40:35 +0800

    按钮计算器
    按钮计算器

---
+++ 10.10/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  10.10
+//
+//  Created by 20161104568 on 2018/10/10.
+//  Copyright © 2018年 20161104568. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var zyj: UITextField!
+    
+    @IBAction func zyj1(_ sender: Any) {
+        
+    }
+    
+    @IBAction func zyj2(_ sender: Any) {
+    }
+    
+    @IBAction func zyj3(_ sender: Any) {
+    }
+    
+    @IBAction func zyj4(_ sender: Any) {
+    }
+    
+    @IBAction func zyj5(_ sender: Any) {
+    }
+    
+    @IBAction func zyj6(_ sender: Any) {
+    }
+    
+    @IBAction func zyj7(_ sender: Any) {
+    }
+    
+    @IBAction func zyj8(_ sender: Any) {
+    }
+    
+    @IBAction func zyj9(_ sender: Any) {
+    }
+    
+    @IBAction func zyjdelete(_ sender: Any) {
+    }
+    
+    @IBAction func zyj0(_ sender: Any) {
+    }
+    
+    @IBAction func zyjadd(_ sender: Any) {
+    }
+    
+    @IBAction func zyjreduce(_ sender: Any) {
+    }
+    
+    @IBAction func zyjcheng(_ sender: Any) {
+    }
+    
+    @IBAction func zyjchu(_ sender: Any) {
+    }
+    
+    @IBAction func zyjequal(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
